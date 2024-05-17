5440c0567a40e74e431e5735929e9bc3d61a5e87
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-05-17 15:00:00 +0530

    explore size of data types in bytes

---
+++ Day-02/data_types.cpp
@@ -0,0 +1,7 @@
+// Day-02/data_types.cpp
+#include <iostream>
+int main() {
+    std::cout << "int size: " << sizeof(int) << " bytes\n";
+    std::cout << "double size: " << sizeof(double) << " bytes\n";
+    return 0;
+}
