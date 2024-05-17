84313cacf1047609d6ea5f6f3291227152fe91e8
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-05-17 13:00:00 +0530

    add float double and char types in variables practice

--- Day-02/variables.cpp
+++ Day-02/variables.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 int main() {
     int age = 20;
-    std::cout << "Age: " << age << "\n";
+    double temp = 98.6;
+    char grade = 'A';
+    std::cout << "Age: " << age << ", Temp: " << temp << ", Grade: " << grade << "\n";
     return 0;
 }
