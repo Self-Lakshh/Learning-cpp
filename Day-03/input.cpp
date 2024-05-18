efbcf48e482e4e265f26546ff71c0ea5d438ebc1
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-05-18 10:00:00 +0530

    learn input using std::cin

---
+++ Day-03/input.cpp
@@ -0,0 +1,9 @@
+// Day-03/input.cpp
+#include <iostream>
+int main() {
+    int score;
+    std::cout << "Enter score: ";
+    std::cin >> score;
+    std::cout << "Score received: " << score << "\n";
+    return 0;
+}
