ce58d1632b3ba47f7a9099c9cb15f5887ee42f63
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-05-20 10:00:00 +0530

    practice arithmetic operators

---
+++ Day-04/operators.cpp
@@ -0,0 +1,8 @@
+// Day-04/operators.cpp
+#include <iostream>
+int main() {
+    int x = 10, y = 3;
+    std::cout << "Addition: " << x + y << "\n";
+    std::cout << "Modulo: " << x % y << "\n";
+    return 0;
+}
