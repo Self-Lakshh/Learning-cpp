219da6f7b8f4fdeb1709950a8932fb9f70fcb02a
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-05-22 13:00:00 +0530

    add nested if conditionals

--- Day-05/conditionals.cpp
+++ Day-05/conditionals.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 int main() {
     int age = 17;
-    if (age >= 18) std::cout << "Adult\n";
-    else std::cout << "Minor\n";
+    if (age >= 18) {
+        std::cout << "Adult\n";
+    } else {
+        if (age >= 13) std::cout << "Teenager\n";
+        else std::cout << "Child\n";
+    }
     return 0;
 }
