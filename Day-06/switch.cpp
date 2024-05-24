1210ccacbe27fd5fa71c0d3b3414c8cd78e40d04
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-05-24 10:00:00 +0530

    practice switch case statement options

---
+++ Day-06/switch.cpp
@@ -0,0 +1,10 @@
+// Day-06/switch.cpp
+#include <iostream>
+int main() {
+    int ch = 1;
+    switch(ch) {
+        case 1: std::cout << "One\n"; break;
+        default: std::cout << "Default\n"; break;
+    }
+    return 0;
+}
