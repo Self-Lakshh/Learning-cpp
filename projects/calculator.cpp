1c11dd2aa46911db228261844689b78cbf5b752a
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-05-27 15:00:00 +0530

    add addition arithmetic branch inside calculator

--- projects/calculator.cpp
+++ projects/calculator.cpp
@@ -4,5 +4,11 @@ int main() {
     int choice;
     std::cout << "1. Add\n2. Exit\nEnter: ";
     std::cin >> choice;
+    if (choice == 1) {
+        int x, y;
+        std::cout << "Enter numbers: ";
+        std::cin >> x >> y;
+        std::cout << "Sum: " << x + y << "\n";
+    }
     return 0;
 }
