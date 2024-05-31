0cffe189b394eb6476d3613c821b42a9e478e5fa
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-05-31 10:00:00 +0530

    learn swap function passing by value

---
+++ Day-10/arguments.cpp
@@ -0,0 +1,13 @@
+// Day-10/arguments.cpp
+#include <iostream>
+void swap(int x, int y) {
+    int t = x;
+    x = y;
+    y = t;
+}
+int main() {
+    int a = 1, b = 2;
+    swap(a, b);
+    std::cout << a << " " << b << "\n"; // doesn't change
+    return 0;
+}
