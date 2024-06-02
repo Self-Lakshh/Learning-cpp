3d21d2b037f74f951e2fd57cd5d845c665dd36e0
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-06-02 13:00:00 +0530

    add subtraction multiply and division functions to calculator v2

--- projects/calculator_v2.cpp
+++ projects/calculator_v2.cpp
@@ -1,7 +1,9 @@
 // projects/calculator_v2.cpp
 #include <iostream>
 int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
+int mul(int a, int b) { return a * b; }
 int main() {
-    std::cout << "Add: " << add(5, 3) << "\n";
+    std::cout << "Sum: " << add(5, 3) << " Sub: " << sub(5, 3) << "\n";
     return 0;
 }
