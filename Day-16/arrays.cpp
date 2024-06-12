32b782ea4e0c075117967075e9146e95b2b4c94b
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-06-12 10:00:00 +0530

    learn array declarations and index print loops

---
+++ Day-16/arrays.cpp
@@ -0,0 +1,9 @@
+// Day-16/arrays.cpp
+#include <iostream>
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    for(int i = 0; i < 5; i++) {
+        std::cout << arr[i] << " ";
+    }
+    return 0;
+}
