b09f3251380dde0ef0ffa42676b3e9e2906a52c4
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-06-16 13:00:00 +0530

    add linear search inside student marks registry

--- projects/student_marks.cpp
+++ projects/student_marks.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 int main() {
     int marks[3] = {85, 90, 78};
-    std::cout << "Marks array instantiated.\n";
+    int search = 90;
+    for(int i=0; i<3; i++) {
+        if (marks[i] == search) {
+            std::cout << "Found marks at student ID: " << i << "\n";
+        }
+    }
     return 0;
 }
