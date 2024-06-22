4cc6d0226c6aced5a04dc9c7e1fc35911a46e8f0
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-06-22 10:00:00 +0530

    learn standard string object modifications and lengths

---
+++ Day-21/std_string.cpp
@@ -0,0 +1,9 @@
+// Day-21/std_string.cpp
+#include <iostream>
+#include <string>
+int main() {
+    std::string s = "C++";
+    s += " Programming";
+    std::cout << s << " size: " << s.size() << "\n";
+    return 0;
+}
