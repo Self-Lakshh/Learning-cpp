e6072d38cc615c55ba1701d62165061045174460
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-06-25 17:00:00 +0530

    add index swap characters routine to reverse string

--- challenges/reverse_string.cpp
+++ challenges/reverse_string.cpp
@@ -1,8 +1,15 @@
 // challenges/reverse_string.cpp
 #include <iostream>
 #include <string>
+void rev(std::string &s) {
+    int n = s.size();
+    for(int i=0; i<n/2; i++) {
+        std::swap(s[i], s[n-i-1]);
+    }
+}
 int main() {
     std::string s = "hello";
-    std::cout << "Original: " << s << "\n";
+    rev(s);
+    std::cout << "Reversed: " << s << "\n";
     return 0;
 }
