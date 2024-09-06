482b70f241f7dd7b932b02f46fc8234d33144897
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-09-06 13:00:00 +0530

    learn stl sorting and binary search algorithms

---
+++ Day-53/stl_algorithms.cpp
@@ -0,0 +1,9 @@
+// Day-53/stl_algorithms.cpp
+#include <iostream>
+#include <vector>
+#include <algorithm>
+int main() {
+    std::vector<int> v = {3, 1, 2};
+    std::sort(v.begin(), v.end());
+    return 0;
+}
