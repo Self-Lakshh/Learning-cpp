2f4bf7b290d2b0ec7242a9704e08b8a3b3d435b6
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-09-09 10:00:00 +0530

    create todo manager project using std::vector and files

---
+++ projects/todo_manager.cpp
@@ -0,0 +1,9 @@
+// projects/todo_manager.cpp
+#include <iostream>
+#include <vector>
+#include <string>
+int main() {
+    std::vector<std::string> todos;
+    todos.push_back("Read C++ book");
+    return 0;
+}
