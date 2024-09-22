2dd797ad7974b40de76341807e1b6a6f65acff4f
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-09-22 13:00:00 +0530

    implement custom singly linked list with cleanup code

--- projects/linked_list.cpp
+++ projects/linked_list.cpp
@@ -4,8 +4,26 @@ struct Node {
     int val;
     Node* next = nullptr;
 };
+class List {
+private:
+    Node* head = nullptr;
+public:
+    void add(int v) {
+        Node* n = new Node{v};
+        n->next = head;
+        head = n;
+    }
+    ~List() {
+        Node* curr = head;
+        while(curr) {
+            Node* nxt = curr->next;
+            delete curr;
+            curr = nxt;
+        }
+    }
+};
 int main() {
-    Node* head = new Node{10};
-    delete head;
+    List l;
+    l.add(5);
     return 0;
 }
