64e3dc138cf084b7d732a0da1d8c1be29ff0184a
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-09-30 13:00:00 +0530

    implement custom binary search tree with inorder traversal

--- projects/binary_search_tree.cpp
+++ projects/binary_search_tree.cpp
@@ -5,6 +5,15 @@ struct Node {
     Node* left = nullptr;
     Node* right = nullptr;
 };
+void print(Node* root) {
+    if(!root) return;
+    print(root->left);
+    std::cout << root->val << " ";
+    print(root->right);
+}
 int main() {
+    Node* root = new Node{10};
+    print(root);
+    delete root;
     return 0;
 }
