651e7ca3ae15378434f4d31f115b49a67a18e5cf
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-11-21 10:00:00 +0530

    refactor banking system project to include safety transaction checks

--- projects/banking_system.cpp
+++ projects/banking_system.cpp
@@ -1,16 +1,11 @@
 // projects/banking_system.cpp
 #include <iostream>
-#include <fstream>
-class Account {
+class Acc {
 public:
     double bal = 100;
+    void draw(double a) {
+        if(a > bal) throw std::runtime_error("overdraw");
+        bal -= a;
+    }
 };
-void save(Account &a) {
-    std::ofstream file("bank.dat", std::ios::binary);
-    file.write((char*)&a, sizeof(a));
-}
-int main() {
-    Account a;
-    save(a);
-    return 0;
-}
+int main() { return 0; }
